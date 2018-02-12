32d9eb7df27a18d74d96183d126af4f433803585
Mark Wanders <mndmwanders@gmail.com>
2018-02-12 21:05:21 +0100

    work in progress

--- app/src/main/java/com/example/mndmw/rincewind/TransactionsActivity.java
+++ app/src/main/java/com/example/mndmw/rincewind/TransactionsActivity.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.AsyncTaskLoader;
 import android.support.v4.content.Loader;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.mndmw.rincewind.domain.Account;
@@ -22,8 +24,10 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TransactionsActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Transaction>> {
+public class TransactionsActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Transaction>>, SwipeRefreshLayout.OnRefreshListener {
 
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+    private ProgressBar mLoadingIndicator;
     private RecyclerView mRecyclerView;
     private TransactionAdapter mTransactionAdapter;
 
@@ -42,7 +46,7 @@ public class TransactionsActivity extends AppCompatActivity implements LoaderMan
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transactions);
 
-        mRecyclerView = (RecyclerView) findViewById(R.id.rv_transactions);
+        mRecyclerView = findViewById(R.id.rv_transactions);
 
         LinearLayoutManager linearLayoutManager
                 = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
@@ -54,7 +58,9 @@ public class TransactionsActivity extends AppCompatActivity implements LoaderMan
 
         mRecyclerView.setAdapter(mTransactionAdapter);
 
-        Intent intentThatStartedThisActivity = getIntent();
+        mLoadingIndicator = findViewById(R.id.loading_indicator);
+
+        final Intent intentThatStartedThisActivity = getIntent();
 
         if (intentThatStartedThisActivity != null) {
             if (intentThatStartedThisActivity.hasExtra(TYPE)) {
@@ -64,6 +70,9 @@ public class TransactionsActivity extends AppCompatActivity implements LoaderMan
                 mID = intentThatStartedThisActivity.getStringExtra(ID);
             }
         }
+
+        mSwipeRefreshLayout = findViewById(R.id.swipe_container);
+        mSwipeRefreshLayout.setOnRefreshListener(this);
 
         LoaderManager.LoaderCallbacks<List<Transaction>> callback = TransactionsActivity.this;
 
@@ -78,6 +87,7 @@ public class TransactionsActivity extends AppCompatActivity implements LoaderMan
             @Override
             protected void onStartLoading() {
                 if (transactions == null) {
+                    mLoadingIndicator.setVisibility(View.VISIBLE);
                     forceLoad();
                 } else {
                     deliverResult(transactions);
@@ -86,25 +96,7 @@ public class TransactionsActivity extends AppCompatActivity implements LoaderMan
 
             @Override
             public List<Transaction> loadInBackground() {
-                URL url;
-                if(args != null && args.containsKey(ID) && args.containsKey(TYPE)) {
-                    url = NetworkUtils.buildUrl(ACCOUNTS, args.getString(TYPE), TRANSACTIONS, args.getString(ID));
-                } else {
-                    url = NetworkUtils.buildUrl(ACCOUNTS, TRANSACTIONS);
-                }
-                String resultString = null;
-                List<Transaction> accounts = new ArrayList<>();
-                try {
-                    resultString = NetworkUtils.getResponseFromHttpUrl(url);
-                    Gson gson = new Gson();
-                    List<Transaction> transactionsResult = gson.fromJson(resultString,  new TypeToken<List<Transaction>>(){}.getType());
-                    if (transactionsResult != null) {
-                        accounts.addAll(transactionsResult);
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                return accounts;
+                return loadTransactions(args);
             }
 
             public void deliverResult(List<Transaction> transactionsResult) {
@@ -116,11 +108,39 @@ public class TransactionsActivity extends AppCompatActivity implements LoaderMan
 
     @Override
     public void onLoadFinished(Loader<List<Transaction>> loader, List<Transaction> data) {
+        mLoadingIndicator.setVisibility(View.INVISIBLE);
         mTransactionAdapter.setTransactionData(data);
     }
 
     @Override
     public void onLoaderReset(Loader<List<Transaction>> loader) {
 
+    }
+
+    @Override
+    public void onRefresh() {
+        loadTransactions(getIntent().getExtras());
+    }
+
+    private List<Transaction> loadTransactions(Bundle args) {
+        URL url;
+        if(args != null && args.containsKey(ID) && args.containsKey(TYPE)) {
+            url = NetworkUtils.buildUrl(ACCOUNTS, args.getString(TYPE), TRANSACTIONS, args.getString(ID));
+        } else {
+            url = NetworkUtils.buildUrl(ACCOUNTS, TRANSACTIONS);
+        }
+        String resultString = null;
+        List<Transaction> accounts = new ArrayList<>();
+        try {
+            resultString = NetworkUtils.getResponseFromHttpUrl(url);
+            Gson gson = new Gson();
+            List<Transaction> transactionsResult = gson.fromJson(resultString,  new TypeToken<List<Transaction>>(){}.getType());
+            if (transactionsResult != null) {
+                accounts.addAll(transactionsResult);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return accounts;
     }
 }

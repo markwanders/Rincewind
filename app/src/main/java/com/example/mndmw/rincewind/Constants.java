642c2bac28c3513ff90e3964fe155be9c52879a4
Mark Wanders <mndmwanders@gmail.com>
2018-09-23 15:10:55 +0200

    changed to production URL

--- app/src/main/java/com/example/mndmw/rincewind/Constants.java
+++ app/src/main/java/com/example/mndmw/rincewind/Constants.java
@@ -1,5 +1,5 @@
 package com.example.mndmw.rincewind;
 
 public class Constants {
-    public static final String BASE_URL = "http://10.0.2.2:8080";
+    public static final String BASE_URL = "https://lavaeolus.herokuapp.com";
 }
